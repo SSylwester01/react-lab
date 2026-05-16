ca5b38e0375672f75c6249bc8bc8684c6a5580ab
Spiewlas <sylwester21@wp.pl>
2026-05-16 10:25:07 +0200

    drugi commit poprawa

--- src/App.js
+++ src/App.js
@@ -8,11 +8,31 @@ function App() {
         setEmail(event.target.value);
     }
 
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    }
+
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}</h2>
             <input type="text" value={email} onChange={handleChange}/>
+
+            {message}
+            <button
+            type="button"
+            onClick={()=>alert(email)}
+            >
+            Wyswietl mój e-mail w alercie
+            </button>
+
+
+
         </div>
     );
 }
